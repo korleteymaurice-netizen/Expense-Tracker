31b23b26bb169db9c353c5d5bc0d8cba9e2bcb4c
Korletey Maurice <korleteymaurice@gmail.com>
2026-09-15 12:02:30 +0000

    Fix secure session cookies behind Vercel proxy

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require('path');
 const pool = require('./db/pool');
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
